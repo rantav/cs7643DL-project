4b6bfa8a0c8f5ac7edbf5b0011f2af46e72bc5de
Ran Tavory <rantav@gmail.com>
2022-12-02 20:15:51 -0500

    Create a chart for accuracy

---
+++ chart.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('data/output/style_transfered/results.csv')
+
+df = df[['style_weight', 'style_accuracy', 'content_accuracy']]
+df.index = df['style_weight']
+df = df.drop(columns=['style_weight'])
+df.plot(style='o-', logx=True)
+plt.show()
