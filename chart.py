bd5b8308e29e7dbc636e15a39f0530b53f95247a
Ran Tavory <rantav@gmail.com>
2022-12-08 23:12:04 +0200

    Add more charts

--- chart.py
+++ chart.py
@@ -1,12 +1,12 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-df = pd.read_csv('data/output/style_transfered/results.csv')
+df = pd.read_csv('data/output/style_transfered/results_gram.csv')
 
 df = df[['style_weight', 'style_accuracy', 'content_accuracy']]
 df.index = df['style_weight']
 df = df.drop(columns=['style_weight'])
 df.plot(style='o-', logx=True)
-plt.suptitle('Style Transfered images Content and Style Accuracy')
-plt.title('with Gram Matrix classifier')
+plt.title('Style Transfered images Content and Style Accuracy\n'
+          'with Gram Matrix classifier')
 plt.show()
